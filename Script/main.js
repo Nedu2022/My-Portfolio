bbc742aadf1f533b58b21c9d5848da544b43a366
Nedu2022 <nwabuokeichinedu00@gmail.com>
2022-11-12 17:00:58 +0100

    FunctionalityV-1.0

--- Script/main.js
+++ Script/main.js
@@ -64,6 +64,26 @@ addEventListener("load", function () {
 });
 
 
+function scrollActive() {
+const scrollY = window.pageYOffset
+
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 58,
+          sectionId = current.getAttribute('id')
+    
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+    }
+  });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+
+
 
 const sr = ScrollReveal({
   origin: 'bottom',
